5f2ffc3723ea8272d4858628a5595482baa3c32a
DWAA1660 <dwatnip123@gmail.com>
2026-02-02 20:02:59 -0700

    fix

--- js/gtaslide.js
+++ js/gtaslide.js
@@ -2,22 +2,22 @@
 //21 April 2014
 
 var image1=new Image()
-image1.src="afghanwide02.JPG" width="930" height="240"
+image1.src="images/afghanwide02.jpg"
 
 var image2=new Image()
-image2.src="29 Palms A Pan.JPG"
+image2.src="images/29 Palms A Pan.JPG"
 
 var image3=new Image()
-image3.src="29 Palms B Pan.JPG"
+image3.src="images/29 Palms B Pan.JPG"
 
 var image4=new Image()
-image4.src="29 Palms C Pan.JPG"
+image4.src="images/29 Palms C Pan.JPG"
 
 var image5=new Image()
-image5.src="Bandito PGLM GLC C CDLB PANORAMIC.JPG"
+image5.src="images/Bandito PGLM GLC C CDLB PANORAMIC.JPG"
 
 var image6=new Image()
-image6.src="Bandito PGLM Launch B CDLB PANORAMIC.JPG"
+image6.src="images/Bandito PGLM Launch B CDLB PANORAMIC.JPG"
 
 
 function gtaslide(){
@@ -26,3 +26,4 @@ function gtaslide(){
 
 
 
+}
